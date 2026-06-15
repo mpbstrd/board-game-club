657b6aef7998762d91ac226cf339d0c3e039505f
mpbstrd <matthewperrybustarde@gmail.com>
2026-06-15 11:00:44 +0800

    feat: add category data and update Category interface in types

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,66 +3,74 @@ export type CurrentUser = {
 }
 
 export type UpcomingEvent = {
-    weekday: string,
-    date: Date,
-    time: string,
-    venue: string,
-    mainGame?: string,
-    mainGameImageUrl?: string,
-    going: number[],
-    maybe: number[],
-    cant: number[],
+    weekday: string
+    date: Date
+    time: string
+    venue: string
+    mainGame?: string
+    mainGameImageUrl?: string
+    going: number[]
+    maybe: number[]
+    cant: number[]
 }
 
 export type Member = {
-    id: number,
-    name: string,
-    sessionsPlayed: number,
-    playedGames: number,
+    id: number
+    name: string
+    sessionsPlayed: number
+    playedGames: number
     avatarUrl: string
+    isAdmin: boolean
 }
 
 export interface Session {
-  sessionNo: number;
-  date: Date;
-  players: number[];
-  games: number[];
-  location: number[];
+  sessionNo: number
+  date: Date
+  players: number[]
+  games: number[]
+  location: number[]
 }
 
 export interface Location {
-  id: number;
-  name: string;
+  id: number
+  name: string
 }
 
 export interface Game {
-  id: number;
-  name: string;
-  category?: string;
-  imgUrl?: string;
+  id: number
+  name: string
+  category?: string
+  imgUrl?: string
+  categories?: Category[]
+}
+
+export interface Category {
+    id: number
+    name: string
+    color?: string
 }
 
 // Session Types
 
 export type TopVenues = {
-    rank: number,
-    name: string,
+    rank: number
+    name: string
     count: number
 }
 
 export type TopGames = {
-    rank: number,
-    name: string,
+    rank: number
+    name: string
     count: number
 }
 
 export type TopMembers = {
-    rank: number,
-    name: string,
+    rank: number
+    name: string
     count: number
 }
 
 export type MonthlySessionCount = {
-    month: string,
+    month: string
     count: number
 }
